e54a9d84fe2e656778d884a64914734f69858aa5
constantin <commandr@gmx.net>
2015-08-26 19:13:03 +0200

    Initial commit

---
+++ desktop/src/com/sidescroller/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.sidescroller.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.sidescroller.game.SideScrollerMainApp;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SideScrollerMainApp(), config);
+	}
+}
